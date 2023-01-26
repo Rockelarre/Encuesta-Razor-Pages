38c612b21f53727163ea76a11800c739e00cb404
cianw <cianw@MACHINELABS>
2023-01-25 22:56:19 -0300

    Agregar archivos de proyecto.

---
+++ EncuestaRazorPages/Pages/Encuesta.cshtml.cs
@@ -0,0 +1,28 @@
+using EncuestaRazorPages.Models;
+using EncuestaRazorPages.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EncuestaRazorPages.Pages
+{
+    public class EncuestaModel : PageModel
+    {
+        private readonly ServicioEncuestas _servicioEncuesta;
+        public EncuestaModel(ServicioEncuestas servicioEncuesta)
+        {
+            _servicioEncuesta = servicioEncuesta;
+        }
+
+        [BindProperty]
+        public Encuesta Encuesta { get; set; }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            _servicioEncuesta.Agregar(Encuesta);
+            return  RedirectToPage("Gracias");
+        }
+    }
+}
